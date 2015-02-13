e2a48091bcb1a638b81d4bd1f04fa21f12cd457e
Alex Lance <code@alexlance.com>
2015-02-13 18:10:10 +1100

    Initial commit

---
+++ gompost.go
@@ -0,0 +1,58 @@
+package gompost
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"bytes"
+)
+
+// Function to make regular application/x-www-form-urlencoded POST requests
+func make_form_request(address string, request map[string]string) (map[string]interface{}, error) {
+	rtn := make(map[string]interface{})
+	u, _ := url.ParseRequestURI(address)
+	client := &http.Client{}
+	input := ""
+	data := url.Values{}
+	for key, value := range request {
+		data.Add(key, value)
+	}
+	input = data.Encode()
+	r, _ := http.NewRequest("POST", fmt.Sprintf("%v", u), bytes.NewBufferString(input))
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(input)))
+	resp, err := client.Do(r)
+	if err != nil {
+		rtn["body"] = resp
+		return rtn, err
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	rtn["body"] = string(body)
+	return rtn, nil
+}
+
+// Function to make application/json requests
+func make_json_request(address string, request map[string]string) (map[string]interface{}, error) {
+	rtn := make(map[string]interface{})
+	u, _ := url.ParseRequestURI(address)
+	client := &http.Client{}
+	input := ""
+	i, _ := json.Marshal(request)
+	input = string(i)
+	r, _ := http.NewRequest("POST", fmt.Sprintf("%v", u), bytes.NewBufferString(input))
+	r.Header.Add("Content-Type", "application/json")
+	r.Header.Add("Content-Length", strconv.Itoa(len(input)))
+	resp, err := client.Do(r)
+	if err != nil {
+		rtn["body"] = resp
+		return rtn, err
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	var jsonPackage map[string]interface{}
+	err2 := json.Unmarshal([]byte(string(body)), &jsonPackage)
+	return jsonPackage, err2
+}
+
